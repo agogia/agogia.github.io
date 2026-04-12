7ff30dde84f36c74fb0845648591c9e8717aebd8
Angad Gogia <agogia24@gmail.com>
2026-04-11 18:44:50 -0700

    Move homepage links into separate file

---
+++ links.js
@@ -0,0 +1,13 @@
+// Edit this list to add, remove, or reorder links on the homepage.
+window.siteLinks = [
+  {
+    href: "http://johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail",
+    title: "Reality has a surprising amount of detail",
+    note: ", an essay by John Salvatier."
+  },
+  {
+    href: "https://marginalrevolution.com/marginalrevolution/2018/10/high-return-activity-raising-others-aspirations.html",
+    title: "A High Return Activity of Raising Others' Aspirations",
+    note: ", a post from Marginal Revolution."
+  }
+];
